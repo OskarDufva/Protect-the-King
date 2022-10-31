26fcc71f03f9aca2aab02c6f539a53cdf8ac2786
Wolfyagrum <Eddyg2525@gmail.com>
2022-10-31 14:02:26 +0100

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _speedTxt;
+    [SerializeField] TextMeshProUGUI _waveTxt;
+
+    private Wavemanager _waveManager;
+    private int _currentWave = 0;
+    private int _gameSpeed = 1;
+    private DragDrop _dragDrop;
+
+    [HideInInspector]
+    public int width;
+    public int height;
+     
+    public Tile _CurrentHoveredTile;
+
+    public TileArray[] Tiles;
+    [SerializeField]public TileArray[] EmptyTiles;
+
+    private void Start()
+    {
+        _waveManager = FindObjectOfType<Wavemanager>();
+        _dragDrop = FindObjectOfType<DragDrop>();
+        WaveTxt();
+    }
+    public void GameSpeedChange()
+    {
+        if (_gameSpeed == 1)
+        {
+            _gameSpeed = 2;
+            _speedTxt.text = ">>";
+            ChangeSpeed();
+        }
+        else if (_gameSpeed == 2)
+        {
+            _gameSpeed = 4;
+            _speedTxt.text = ">>>";
+            ChangeSpeed();
+        }
+        else if (_gameSpeed == 4)
+        {
+            _gameSpeed = 1;
+            _speedTxt.text = ">";
+            ChangeSpeed();
+        }
+    }
+
+    private void ChangeSpeed()
+    {
+        Time.timeScale = _gameSpeed;
+    }
+
+    public void NextWave()
+    {
+        if (_waveManager.EnemiesAlive <= 0)
+        {
+            _waveManager.SpawnWave(_currentWave);
+            WaveTxt();
+            _currentWave++;
+        }
+    }
+
+    private void WaveTxt()
+    {
+        int value = _currentWave + 1;
+        _waveTxt.text = "Wave: " + value + "/" + _waveManager.WaveAmountCounter().ToString();
+    }
+
+    public void HighlightUnoccupiedTiles()
+    {
+        for (int x = 0; x < Tiles.Length; x++)
+        {
+            for (int y = 0; y < Tiles[0].Tiles.Length; y++)
+            {
+                if (Tiles[x].Tiles[y] != _CurrentHoveredTile)
+                {
+                    if (Tiles[x].Tiles[y].EnemyPathTile == false && Tiles[x].Tiles[y].OccupiedTile == false)
+                    {
+                        Tiles[x].Tiles[y].ValidPlacementColor();
+                    }
+                    else
+                    {
+                        Tiles[x].Tiles[y].InvalidPlacementColor();
+                    }
+                }                
+            }
+        }
+    }
+
+    public void ResetColors()
+    {
+        for (int x = 0; x < Tiles.Length; x++)
+        {
+            for (int y = 0; y < Tiles[0].Tiles.Length; y++)
+            {
+                    Tiles[x].Tiles[y].OriginalColor();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/GridManager/GridManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width;
+    [SerializeField] private int _height;
+
+    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Transform _cam;
+
+    [ContextMenu("Generate Grid")]
+    public void GenerateGrid()
+    {
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.Tiles = new TileArray[_width];
+
+        for (int i = 0; i < gameManager.Tiles.Length; i++)
+        {
+            gameManager.Tiles[i].Tiles = new Tile[_height];
+        }
+
+        PathFinding pathFinding = FindObjectOfType<PathFinding>();
+        pathFinding._tiles.Clear();
+
+        gameManager.width = _width;
+        gameManager.height = _height;
+        
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.Index = new Vector2Int(x, y);
+
+                gameManager.Tiles[x].Tiles[y] = spawnedTile;
+
+                pathFinding._tiles.Add(spawnedTile);
+
+                spawnedTile.transform.parent = this.transform;
+                spawnedTile.name = $"Tile {x}, {y}";
+                if (spawnedTile.TileTxt != null)
+                {
+                    spawnedTile.TileTxt.GetComponent<TextMeshPro>().text = spawnedTile.name;
+                }
+            }
+        }
+        _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+
+}
+
+[System.Serializable]
+public struct TileArray
+{
+    public Tile[] Tiles;
+}
